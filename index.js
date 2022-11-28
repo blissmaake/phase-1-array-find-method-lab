6ff7754bf7a877d04a5efeac081911f5feb3c5c8
Bliss <blissmaake140@gmail.com>
2022-11-28 07:49:39 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin (result) {
+    for (const wonYear of result) {
+        if (wonYear.result === "W") {
+            return wonYear.year;
+        }
+    }
+}
